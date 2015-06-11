a1ffb468b2bac6184a621eafb759d09445d7602b
robin1315 <robin1315@onet.pl>
2015-06-11 23:16:16 +0200

    2

--- PW_1_2/PW_1_2/Program.cs
+++ PW_1_2/PW_1_2/Program.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PW_1_2
 {
     class Program
     {
+        private static int[,] graph;
+        private static int wierzcholki = 100;
+
         static void Main(string[] args)
         {
+            int numberOfThreads = 0;
+            Thread thread;
+            try
+            {
+                while (true){
+                    numberOfThreads++;
+                    thread = new Thread(new ThreadStart(CreateGraph));
+                    thread.Start();
+                    Console.WriteLine(numberOfThreads);
+                }
+            }
+            catch{
+                Console.WriteLine("Error!!");
+                Console.WriteLine("Liczba watków:" + (numberOfThreads - 1));
+                Console.ReadKey();
+                return;
+            }
 
+        }
+
+        private static void CreateGraph(){
+            graph = new int[wierzcholki, wierzcholki];
+            Random ran = new Random();
+
+            for (int i = 0; i < wierzcholki; i++){
+                graph[i, i] = 0;
+            }
+
+            for (int i = 0; i < wierzcholki; i++){
+                for (int j = 0; j < wierzcholki; j++){
+                    graph[i, j] = ran.Next(0, 2);
+                    graph[j, i] = graph[i, j];
+                }
+            }
         }
     }
 }
